f38c2610c548af04017d254d4fb2837a7960fedd
Henry Feng <henryfeng12345@gmail.com>
2022-01-11 10:36:14 -0600

    fix shell command

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ var helpers = require("./lib/helpers");
 var shell = require('shelljs');
 
 var CronJob = require('cron').CronJob;
-var job = new CronJob('1 * * * * *', function() {
+var job = new CronJob('00 00 11 * * *', function() {
   const d = new Date();
   console.log('11:00 AM: job run:', d);
   // Update db 
-  shell.exec('cd stake-o-matic-master && export VALIDATORS_APP_TOKEN=5TFNgpCnuRZz6mkk3oyNFbin && bash clean-score-all-mainnet.sh',
+  shell.exec('cd stake-o-matic-master && cargo build && export VALIDATORS_APP_TOKEN=5TFNgpCnuRZz6mkk3oyNFbin && bash clean-score-all-mainnet.sh',
     // Delete current validators json
     fs.unlink('./.data/epochs/validators.json', (err) => {
     if (err) throw err;
@@ -31,7 +31,7 @@ var job = new CronJob('1 * * * * *', function() {
 });
 job.start();
 
-// shell.exec('cd stake-o-matic-master && export VALIDATORS_APP_TOKEN=5TFNgpCnuRZz6mkk3oyNFbin && bash clean-score-all-mainnet.sh',helpers.generateValidators());
+// shell.exec('cd stake-o-matic-master && cargo build && export VALIDATORS_APP_TOKEN=5TFNgpCnuRZz6mkk3oyNFbin && bash clean-score-all-mainnet.sh',helpers.generateValidators());
 
 const PORT = process.env.PORT || 8080;
 // Instantiate the HTTP server
