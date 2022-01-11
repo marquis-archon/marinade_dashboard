28f3197abe1f35a1aeca8f8ba913e8f16a68647a
Henry Feng <henryfeng12345@gmail.com>
2022-01-11 01:39:43 -0600

    add console log for job run start

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ var shell = require('shelljs');
 
 var CronJob = require('cron').CronJob;
 var job = new CronJob('00 00 11 * * *', function() {
+  const d = new Date();
+  console.log('11:00 AM: job run:', d);
   // Update db 
   shell.exec('cd stake-o-matic-master && export VALIDATORS_APP_TOKEN=5TFNgpCnuRZz6mkk3oyNFbin && bash clean-score-all-mainnet.sh',
     // Delete current validators json
